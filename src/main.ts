5f920732eae9210a66cf0c076e5291d904dd776f
maximkoltovich <12345kolt@gmail.com>
2023-07-23 15:05:18 +0300

    доработки

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 // @ts-ignore
 import App from './App.vue'
 // @ts-ignore
 import vuetify from './plugins/vuetify'
 // @ts-ignore
-import { loadFonts } from './plugins/webfontloader'
+import {loadFonts} from './plugins/webfontloader'
 
 loadFonts()
 
 createApp(App)
-  .use(vuetify)
-  .mount('#app')
+    .use(vuetify, {
+        iconfont: 'md' || 'fa' || 'mdi'
+    })
+    .mount('#app')
