7e462a57b6f1dc078f18db6fbb84720c21ed78c0
maximkoltovich <12345kolt@gmail.com>
2023-11-16 16:50:01 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 import vue from '@vitejs/plugin-vue';
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -6,9 +6,9 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	],
-	base: `/portfolio-project/`
+    base: 'portfolio-project',
+    plugins: [
+        vue(),
+        vuetify({autoImport: true}),
+    ],
 })
